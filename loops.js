df68bdca7d2e0fae807c2de1fdd3ee8faa35cff9
Steven C Eaton <steveeaton5@gmail.com>
2018-08-22 21:35:38 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,8 +1,6 @@
 function forLoop(array){
   for(let i = 0; i < 25; i++ ){
-    if(i === 1){
-      array.push(`"I am ${i} strange loop${i === 1 ? '' : 's'}."`)} else{
-      array.push(`"I am ${i} strange loop${i === 1 ? '' : 's'}."`)}
+      array.push(`"I am ${i} strange loop${i === 1 ? '' : 's'}."`);
   }
   return array;
 }
